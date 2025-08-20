0fa5de291f2d4600e07857b738c556fb312e9d1c
Kozma Kristof <bechryko@gmail.com>
2025-08-20 10:03:51 +0200

    NGEN-63 Clean up old error handling

--- src/app/shared/error-handling/error-message-id.ts
+++
@@ -1,4 +0,0 @@
-import { ErrorMessages } from "./error-messages";
-import { ErrorSource } from "./error-source";
-
-export type ErrorMessageId<T extends ErrorSource> = keyof (typeof ErrorMessages)[T];
--- src/app/shared/error-handling/error-source.ts
+++
@@ -1,3 +0,0 @@
-import { ErrorMessages } from "./error-messages";
-
-export type ErrorSource = keyof typeof ErrorMessages;
