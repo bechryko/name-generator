870972971ad11bb6f764b97ee84836cdd01ac40a
Kozma Kristof <bechryko@gmail.com>
2025-09-01 18:22:03 +0200

    NGEN-105 Add loading indicator to bulk generation
    Also increase max generated name count

--- src/app/generation/bulk-generation-dialog/bulk-generation-dialog.component.ts
+++ src/app/generation/bulk-generation-dialog/bulk-generation-dialog.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, ElementRef, inject, signal, viewChild } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { GenerationUtils } from "@ngen-generation/utils";
 import { InteractiveIconComponent } from "@ngen-shared/components";
 import { BulkGenerationDialogData } from "./bulk-generation-dialog-data";
@@ -9,7 +10,7 @@ import { BulkGenerationWorkerUtils } from "./worker/bulk-generation.worker.utils
 
 @Component({
    selector: "ngen-bulk-generation-dialog",
-   imports: [MatButtonModule, InteractiveIconComponent],
+   imports: [MatButtonModule, InteractiveIconComponent, MatProgressSpinnerModule],
    templateUrl: "./bulk-generation-dialog.component.html",
    styleUrl: "./bulk-generation-dialog.component.scss",
    changeDetection: ChangeDetectionStrategy.OnPush
@@ -18,14 +19,16 @@ export class BulkGenerationDialogComponent {
    private readonly data: BulkGenerationDialogData = inject(MAT_DIALOG_DATA);
    private readonly dialogRef = inject(MatDialogRef);
 
+   public readonly maxGenerationTimes = 2_000;
    public readonly generationTimes = signal(5);
    public readonly generatedNames = signal<GeneratedName[]>([]);
+   public readonly isLoading = signal(false);
    public readonly generatedNamesContainerRef = viewChild.required<ElementRef<HTMLElement>>("generatedNamesContainer");
    private readonly worker = this.initWorker();
 
    public onGenerationTimesChange(event: any): void {
       const value: number = event.target.value;
-      const clampedRoundedValue = Math.max(1, Math.min(100, Math.round(value)));
+      const clampedRoundedValue = Math.max(1, Math.min(this.maxGenerationTimes, Math.round(value)));
       this.generationTimes.set(clampedRoundedValue);
    }
 
@@ -36,6 +39,7 @@ export class BulkGenerationDialogComponent {
          configJSON: GenerationUtils.configToJSON(this.data.config)
       };
 
+      this.isLoading.set(true);
       if (this.worker) {
          this.worker.postMessage(workerData);
       } else {
@@ -53,6 +57,7 @@ export class BulkGenerationDialogComponent {
 
    private onGenerationComplete(names: GeneratedName[]): void {
       this.generatedNames.update(oldNames => [...oldNames, ...names]);
+      this.isLoading.set(false);
 
       if (this.isScrolledToBottom) {
          setTimeout(() => this.scrollToBottom(), 0);
