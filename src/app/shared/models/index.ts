27c506a40741e06f6d2ba4f465a242de3638c905
Kozma Kristof <bechryko@gmail.com>
2025-09-03 17:22:09 +0200

    NGEN-114 Use .d.ts files instead of .ts where possible

--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -1,6 +1,6 @@
 //@index(["./*", "!./*.spec.ts"], (f, _) => `export { ${_.pascalCase(f.name.split(".")[0])} } from "${f.path}";`)
 export { LetterSet } from "./letter-set";
-export { NgenSidebarSelectable } from "./ngen-sidebar-selectable";
+export { NgenSidebarSelectable } from "./ngen-sidebar-selectable.d";
 export { RegularCharacter } from "./regular-character";
 export { RegularLetterSet } from "./regular-letter-set";
 export { RegularReference } from "./regular-reference";
