7e6ca5f645ef34291af21592d65c98dd3d947b36
Kozma Kristof <bechryko@gmail.com>
2025-09-03 17:31:30 +0200

    NGEN-114 Add unit tests for deleteChar

---
+++ src/app/shared/functions/delete-char.spec.ts
@@ -0,0 +1,16 @@
+import { deleteChar } from "./delete-char";
+
+describe("deleteChar", () => {
+   it("should delete character from valid position", () => {
+      expect(deleteChar("asd", 1)).toEqual("ad");
+      expect(deleteChar("qwertzuiop", 5)).toEqual("qwertuiop");
+      expect(deleteChar("lkj", 2)).toEqual("lk");
+      expect(deleteChar("f", 0)).toEqual("");
+   });
+
+   it("should not change the string if the position is invalid", () => {
+      expect(deleteChar("asd", 4)).toEqual("asd");
+      expect(deleteChar("vbn", -1)).toEqual("vbn");
+      expect(deleteChar("", 0)).toEqual("");
+   });
+});
--- src/app/shared/functions/index.ts
+++ src/app/shared/functions/index.ts
@@ -1,4 +1,4 @@
-//@index("./*", (f, _) => `export { ${_.camelCase(f.name)} } from "${f.path}";`)
+//@index(["./*", "!./*.spec.ts"], (f, _) => `export { ${_.camelCase(f.name)} } from "${f.path}";`)
 export { deleteChar } from "./delete-char";
 export { getCharacterContainer } from "./get-character-container";
 export { last } from "./last";
